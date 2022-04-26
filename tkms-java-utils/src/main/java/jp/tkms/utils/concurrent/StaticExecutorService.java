89f7052361029c8a5571403048fcb4ac151175c1
Shunki Takami <s-takami@tkms.jp>
2022-04-26 16:03:35 +0900

    fix StaticExecutorService

--- tkms-java-utils/src/main/java/jp/tkms/utils/concurrent/StaticExecutorService.java
+++ tkms-java-utils/src/main/java/jp/tkms/utils/concurrent/StaticExecutorService.java
@@ -6,13 +6,17 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class StaticExecutorService {
+    private static boolean hookAdded = false;
     private static ObjectWrapper<ExecutorService> executorService = new ObjectWrapper<>();
 
     public static ExecutorService get() {
         synchronized (executorService) {
             if (executorService.get() == null || (executorService.get() != null && executorService.get().isShutdown())) {
                 executorService.set(Executors.newWorkStealingPool());
-                Runtime.getRuntime().addShutdownHook(new Thread(() -> executorService.get().shutdown()));
+                if (!hookAdded) {
+                    hookAdded = true;
+                    Runtime.getRuntime().addShutdownHook(new Thread(() -> executorService.get().shutdown()));
+                }
             }
             return executorService.get();
         }
