d58bb0655a32d916c80586e495c88c5ef3edfb43
Shunki Takami <s-takami@tkms.jp>
2022-05-23 16:55:42 +0900

    fix SafeConcurrentHashMap

--- tkms-java-utils/src/main/java/jp/tkms/utils/concurrent/SafeConcurrentHashMap.java
+++ tkms-java-utils/src/main/java/jp/tkms/utils/concurrent/SafeConcurrentHashMap.java
@@ -4,14 +4,14 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class SafeConcurrentHashMap<K, V> extends ConcurrentHashMap<K, V> {
-    public V get(K key, Callable<V> defaults) throws Exception {
+    public V get(Object key, Callable<V> defaults) throws Exception {
         V value = get(key);
         if (value == null) {
             synchronized (this) {
                 value = get(key);
                 if (value == null) {
                     value = defaults.call();
-                    put(key, value);
+                    put((K)key, value);
                 }
             }
         }
