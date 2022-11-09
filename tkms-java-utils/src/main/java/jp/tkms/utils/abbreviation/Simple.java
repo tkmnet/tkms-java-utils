74512554193ab00b0085c22a057da668262d4492
Shunki Takami <s-takami@tkms.jp>
2022-11-09 20:58:24 +0900

    add printSimpleStackTrace

--- tkms-java-utils/src/main/java/jp/tkms/utils/abbreviation/Simple.java
+++ tkms-java-utils/src/main/java/jp/tkms/utils/abbreviation/Simple.java
@@ -23,4 +23,29 @@ public class Simple {
       //NOP
     }
   }
+
+  public static void printSimpleStackTrace(Throwable e) {
+    StringBuilder builder = new StringBuilder();
+    builder.append(e.getClass().getSimpleName());
+    builder.append('[');
+    builder.append(e.getMessage());
+    builder.append(']');
+    String prevClassName = "";
+    for (StackTraceElement element : e.getStackTrace()) {
+      builder.append("->(");
+      if (prevClassName.equals(element.getClassName())) {
+        builder.append("\".");
+        builder.append(element.getMethodName());
+      } else {
+        builder.append(element.getClassName().replaceFirst(".*\\.", ""));
+        builder.append('.');
+        builder.append(element.getMethodName());
+        prevClassName = element.getClassName();
+      }
+      builder.append(':');
+      builder.append(element.getLineNumber());
+      builder.append(')');
+    }
+    System.err.println(builder.toString());
+  }
 }
